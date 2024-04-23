315b8e925246dd6c14af74d30fc058c05cecf0b8
zhikeyan <742928766@qq.com>
2024-04-23 20:44:33 +0800

    Logger类的实现

--- sylar/log.h
+++ sylar/log.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <stdint.h>
 #include <memory>
+#include <list>
 namespace sylar{
 
 // 日志事件
@@ -56,9 +57,6 @@ private:
 };
 
 
-
-
-
 // 日志输出器
 class Logger{
 public:
@@ -67,10 +65,20 @@ public:
     Logger(const std::string& name = "root");
 
     void log(LogLevel::Level level, LogEvent::ptr event);
+
+    void debug(LogEvent::ptr event);
+    void info(LogEvent::ptr event);
+    void warn(LogEvent::ptr event);
+    void error(LogEvent::ptr event);
+    void fatal(LogEvent::ptr event);
+    void addAppender(LogAppender::ptr appender);
+    void delAppender(LogAppender::ptr appender);
+    LogLevel::Level getLevel() const {return m_level;}
+    void setLevel(LogLevel::Level level) {m_level = level;}
 private:
-    std::string m_name;
-    LogLevel::Level m_level;
-    LogAppender::ptr m_logAppender;
+    std::string m_name; // 日志名称
+    LogLevel::Level m_level; // 日志级别
+    std::list<LogAppender::ptr> m_appenders; // Appender集合
 
 };
 
